8f67cba716d8a50353edf5b9306abea30c5adf27
Bryam Diaz <bsdiaza@unal.edu.co>
2024-01-25 19:38:54 -0500

    Rescue Api REST initialization commit

---
+++ src/main/java/org/meli/resistance/rescueapi/RescueApiApplication.java
@@ -0,0 +1,13 @@
+package org.meli.resistance.rescueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/meli/resistance/rescueapi/RescueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meli.resistance.rescueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
